386ef2159ad0a5eeba50c9c2c86f6cbb3e7365f9
sawyerrick <1103031078@qq.com>
2023-06-16 02:30:47 +0800

    rppl will show a monkey face when meeting an error

--- 02/src/monkey/repl/repl.go
+++ 02/src/monkey/repl/repl.go
@@ -30,8 +30,26 @@ func Start(in io.Reader, out io.Writer) {
         io.WriteString(out, "\n")
     }
 }
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
 func printParserErrors(out io.Writer, errors []string) {
-    for _, msg := range errors {
-        io.WriteString(out, "\t"+msg+"\n")
-    }
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
+
