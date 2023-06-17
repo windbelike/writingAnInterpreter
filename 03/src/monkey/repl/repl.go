e1aabfd59ad2cf14189a50e5705ca35622ae13c3
sawyerrick <1103031078@qq.com>
2023-06-18 02:21:33 +0800

    eval

--- 03/src/monkey/repl/repl.go
+++ 03/src/monkey/repl/repl.go
@@ -4,31 +4,36 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+
+	"sawyer.com/v3/src/monkey/evaluator"
 	"sawyer.com/v3/src/monkey/lexer"
-    "sawyer.com/v3/src/monkey/parser"
+	"sawyer.com/v3/src/monkey/parser"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
-    scanner := bufio.NewScanner(in)
-    for {
-        fmt.Printf(PROMPT)
-        scanned := scanner.Scan()
-        if !scanned {
-            return
-        }
-        line := scanner.Text()
-        l := lexer.New(line)
-        p := parser.New(l)
-        program := p.ParseProgram()
-        if len(p.Errors()) != 0 {
-            printParserErrors(out, p.Errors())
-            continue
-        }
-        io.WriteString(out, program.String())
-        io.WriteString(out, "\n")
-    }
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
 }
 
 const MONKEY_FACE = `            __,__
@@ -52,4 +57,3 @@ func printParserErrors(out io.Writer, errors []string) {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
-
