0c9af017c649975d6c83431d9d0c526ebd0d1226
sawyerrick <1103031078@qq.com>
2023-06-18 02:07:55 +0800

    eval

--- 03/src/monkey/object/object.go
+++ 03/src/monkey/object/object.go
@@ -7,6 +7,7 @@ import (
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 type ObjectType string
@@ -27,4 +28,26 @@ func (i *Integer) Inspect() string {
 }
 func (i *Integer) Type() ObjectType {
 	return INTEGER_OBJ
+}
+
+// boolean is an object
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
+
+// null is an object
+type Null struct{}
+
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
+func (n *Null) Inspect() string {
+	return "null"
 }
