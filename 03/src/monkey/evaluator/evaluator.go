2773093a8e1b821b8945dba53b46561c77208bcb
sawyerrick <1103031078@qq.com>
2023-06-19 02:12:43 +0800

    Infix expression evaluation

--- 03/src/monkey/evaluator/evaluator.go
+++ 03/src/monkey/evaluator/evaluator.go
@@ -90,12 +90,17 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBoolToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBoolToBooleanObject(left != right)
 	default:
 		return NULL
 	}
 }
 
-func evalIntegerInfixExpression(operator string,
+func evalIntegerInfixExpression(
+	operator string,
 	left, right object.Object,
 ) object.Object {
 	leftVal := left.(*object.Integer).Value
@@ -109,6 +114,14 @@ func evalIntegerInfixExpression(operator string,
 		return &object.Integer{Value: leftVal * rightVal}
 	case "/":
 		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBoolToBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
 	}
